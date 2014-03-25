c8a27ed6faffdeef4e55bfe598117779b0b70b91
jvandermolen <jvandermm@gmail.com>
2014-03-24 22:40:39 -0300

    fixed lambda/statistics.py

--- lambda/statistics.py
+++ lambda/statistics.py
@@ -6,12 +6,16 @@ resultsfile = 'results_' + albumname + '.pkl'
 
 results = pickle.load(open(resultsfile, 'rb'))
 
-for r in results:
-    if not 'photos' in r.body.keys():
-        r.body['photos'] = [{'tags':[{'uids':[{'predicted':'no recognition'}]}]}]
+actual = range(len(results))
+predicted = range(len(results))
+for i in xrange(len(results)):
+    r = results[i]
+    actual[i] = r.body['actualid']
+    try:
+        predicted[i] = r.body['photos'][0]['tags'][0]['uids'][0]['predicted']
+    except:
+        predicted[i] = '-1'
 
-ids = [ ( r.body['actualid'], [r.body['photos'][0]['tags'][0]['uids'][0]['predicted'] ) for r in results ]
-actual, predicted = zip(*ids)
 accuracy =  np.mean( actual == predicted )*100
 
 print 'acierto en la prediccion: ' + str(accuracy) + '%'
